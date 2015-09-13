a07e4ddcb12122bca57865d577ef641cb2196e31
Brad Harrington <bnharrington@gmail.com>
2015-09-13 12:45:54 -0700

    First attempt at adding Assignment 1

---
+++ js/script.js
@@ -0,0 +1,203 @@
+//STEP 1
+//var someMonth;
+//function(theMonth);		// function to return current month
+//const CURRENTMONTH; 	// a constant
+//var summerMonths; 		// an array of summer months
+//myLibraryFunction;     // a function 
+//END STEP 1
+
+//STEP2
+//a numeric literal expression
+//7.5
+
+//a string literal expression
+//"Goodbye!"
+
+//a Boolean literal expression
+//true
+
+//a null literal expression.
+//var undefinedObject;
+
+//END STEP2
+
+//STEP3
+//Complex Variable Expressions
+//var complexExpression = (17 * 6/3 + 6);
+//var myTeamMemberr = new person("Dave", "Smith", 40, "designer");
+
+//END STEP3
+
+//STEP4
+/*var strFirstName;
+var strLastName;
+var numAddress;
+var strCity;
+var strState;
+var numZipCode;
+var numAge;
+var strRefSource;*/
+
+//END STEP4
+
+//STEP5
+/*var strFirstName = "Brad";
+var strLastName = "Harrington";
+var strAddress = "12345 First Street";
+var strCity = "San Diego";
+var strState = "CA";
+var strZipCode = "92037";
+var numAge = "40";
+var strRefSource = "wiki";*/
+
+//END STEP5
+
+//STEP6
+//Coercian: adding a number and a string
+//var one = ("Brad" + 5);
+//console.log(one);
+
+//Coercian: adding a boolean and a string
+//var primitiveTrue = true;
+//var two = ("Brad" + primitiveTrue);
+//console.log(two);
+
+//Coercian: adding boolean and number
+//var primitiveTrue = true;
+//var three = 5 + primitiveTrue;
+//console.log(three);
+
+//END STEP6
+
+//STEP7
+//var car;
+//console.log(car);
+//car = "Chevy";
+
+//END STEP7
+
+//STEP8
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+//END STEP8
+
+//STEP9
+//var nada = null;
+//var yada;
+//console.log(nada);
+//console.log(yada);
+
+//END STEP9
+
+//STEP10
+//string, number, Boolean, object, and undefined.
+//console.log(typeof "Brad"); //string
+//console.log(typeof 40); //number
+//var doodle = false;
+//console.log(typeof doodle); //boolean
+//console.log(typeof new Object()); //object
+//console.log(typeof myName); //undefined
+
+//END STEP10
+
+//STEP11
+//alert("Hello " + "Brad Harrington" + ", " + "welcome to the JavaScript class!");
+
+//END STEP11
+
+//STEP12
+//var name = "Brad Harrington";
+//alert("Hello " + name + ", " + "welcome to the JavaScript class!");
+
+//END STEP12
+
+//STEP13
+//var name = "Brad Harrington";
+//var course = "JavaScript";
+//alert("Hello " + name + ", " + "welcome to the " + course + " class!");
+
+//END STEP13
+
+//STEP14
+//var name = "Brad Harrington";
+//var course = "JavaScript";
+//alert("Hello " + name + ", " + "\nWelcome to the " + course + " class!");
+
+//END STEP14
+
+//STEP15
+//var name = prompt("Please enter your full name:");
+//var course = "JavaScript";
+//alert("Hello " + name + ", " + "\nWelcome to the " + course + " class!");
+
+
+//END STEP15
+
+//STEP16
+//var name = prompt("Please enter your full name:");
+//var course = prompt("Please enter the class name:");
+//alert("Hello " + name + ", " + "\nWelcome to the " + course + " class!");
+
+//END STEP16
+
+//STEP17
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+
+//END STEP17
+
+//STEP18
+//var x = 20;
+//x=x+20;
+//console.log(x);
+
+//END STEP18
+
+//STEP19
+//var x = 20;
+//x=x*5;
+//console.log(x);
+
+
+//END STEP19
+
+//STEP20
+//var x = 20%3;
+//x=x/1;
+//console.log(x);
+
+//END STEP20
+
+//STEP21
+//var x = 20;
+//var y = 20;
+//console.log(x==y);
+
+//END STEP21
+
+//STEP22
+//var x = 20;
+//var y = 30;
+//console.log(x==y);
+
+//END STEP22
+
+//STEP23
+//widget = new Object;
+//console.log(typeof widget);
+
+//END STEP23
+
+//STEP24
+//widget = new Object;
+//console.log(widget instanceof Object);
+
+//END STEP24
+
+//STEP25
+//widget = new Object;
+//console.log(widget instanceof Array);
+
+//END STEP25
